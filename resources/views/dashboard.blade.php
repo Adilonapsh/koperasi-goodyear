8afe6026a8eac4d2c151ba2bd8a3d53ec6054fa3
Adilonapsh <lubos418@gmail.com>
2024-02-17 02:10:02 +0700

    Dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -13,41 +13,69 @@
                 <h4 class="text-2xl font-bold">{{ Auth::user()->name }}.</h4>
             </div>
             <div class="bg-gray-50 p-10 rounded-lg shadow-lg">
-                <div class="grid grid-cols-3 gap-3">
+                <div class="grid grid-cols-2 lg:grid-cols-3 gap-3">
                     <a href="{{ route('status-dana-segar.index') }}" class="hover:shadow-lg transition-all">
-                        <div class="flex bg-gray-200 h-[200px] items-center text-center justify-center shadow-lg">
-                            <p>Status Dana Segar</p>
+                        <div
+                            class="flex bg-gray-200 py-10 h-[150px] lg:h-[200px] items-center text-center justify-center shadow-lg">
+                            <div>
+                                <i class="fa-solid fa-square-poll-horizontal text-4xl lg:text-5xl text-green-500"></i>
+                                <p>Status Dana Segar</p>
+                            </div>
                         </div>
                     </a>
                     <a href="{{ route('potongan-koperasi.index') }}" class="hover:shadow-lg transition-all">
-                        <div class="flex bg-gray-200 h-[200px] items-center text-center justify-center shadow-lg">
-                            <p>Potongan Koperasi</p>
+                        <div
+                            class="flex bg-gray-200 py-10 h-[150px] lg:h-[200px] items-center text-center justify-center shadow-lg">
+                            <div>
+                                <i class="fa-solid fa-percent text-4xl lg:text-5xl text-red-500"></i>
+                                <p>Potongan Koperasi</p>
+                            </div>
                         </div>
                     </a>
                     <a href="{{ route('plafon-konsumsi.index') }}" class="hover:shadow-lg transition-all">
-                        <div class="flex bg-gray-200 h-[200px] items-center text-center justify-center shadow-lg">
-                            <p>Plafon Konsumsi</p>
+                        <div
+                            class="flex bg-gray-200 py-10 h-[150px] lg:h-[200px] items-center text-center justify-center shadow-lg">
+                            <div>
+                                <i class="fa-solid fa-cart-arrow-down text-4xl lg:text-5xl text-blue-700"></i>
+                                <p>Plafon <br> Konsumsi</p>
+                            </div>
                         </div>
                     </a>
                     <a href="{{ route('transaksi-konsumsi.index') }}" class="hover:shadow-lg transition-all">
-                        <div class="flex bg-gray-200 h-[200px] items-center text-center justify-center shadow-lg">
-                            <p>Transaksi Konsumsi</p>
+                        <div
+                            class="flex bg-gray-200 py-10 h-[150px] lg:h-[200px] items-center text-center justify-center shadow-lg">
+                            <div>
+                                <i class="fa-solid fa-right-left text-4xl lg:text-5xl text-emerald-500"></i>
+                                <p>Transaksi Konsumsi</p>
+                            </div>
                         </div>
                     </a>
                     <a href="{{ route('barang.index') }}" class="hover:shadow-lg transition-all">
-                        <div class="flex bg-gray-200 h-[200px] items-center text-center justify-center shadow-lg">
-                            <p>Harga Barang</p>
+                        <div
+                            class="flex bg-gray-200 py-10 h-[150px] lg:h-[200px] items-center text-center justify-center shadow-lg">
+                            <div>
+                                <i class="fa-solid fa-money-bill text-4xl lg:text-5xl text-rose-600"></i>
+                                <p>Harga Barang</p>
+                            </div>
                         </div>
                     </a>
                     <a href="{{ route('simpanan.index') }}" class="hover:shadow-lg transition-all">
-                        <div class="flex bg-gray-200 h-[200px] items-center text-center justify-center shadow-lg">
-                            <p>Simpanan</p>
+                        <div
+                            class="flex bg-gray-200 py-10 h-[150px] lg:h-[200px] items-center text-center justify-center shadow-lg">
+                            <div>
+                                <i class="fa-solid fa-wallet text-4xl lg:text-5xl text-blue-700"></i>
+                                <p>Simpanan</p>
+                            </div>
                         </div>
                     </a>
-                    @hasanyrole("admin")
+                    @hasanyrole('admin')
                         <a href="{{ route('upload-harian.index') }}" class="hover:shadow-lg transition-all">
-                            <div class="flex bg-gray-200 h-[200px] items-center text-center justify-center shadow-lg">
-                                <p>Update Harian</p>
+                            <div
+                                class="flex bg-gray-200 py-10 h-[150px] lg:h-[200px] items-center text-center justify-center shadow-lg">
+                                <div>
+                                    <i class="fa-solid fa-download text-4xl lg:text-5xl"></i>
+                                    <p>Update Harian</p>
+                                </div>
                             </div>
                         </a>
                     @endhasanyrole
--- resources/views/livewire/data-transaksi-konsumsi-layouts.blade.php
+++ resources/views/livewire/data-transaksi-konsumsi-layouts.blade.php
@@ -12,18 +12,20 @@
                     @foreach ($fjkrDetail as $fjkr)
                         @if (count($fjkr->fjkrDetail) !== 0)
                             <tr class="my-2">
-                                <th class="bg-green-300 border border-green-400 py-3" colspan="3">{{ $fjkr->no_fj }} {{ $fjkr->tgl_fj }}</th>
+                                <th class="bg-green-300 border border-green-400 py-3" colspan="3">{{ $fjkr->no_fj }}
+                                    {{ $fjkr->tgl_fj }}</th>
                             </tr>
                             @foreach ($fjkr->fjkrDetail as $key => $detail)
                                 <tr>
                                     <td class="px-4 border">
                                         {{ $detail->jumlah_barang }}
-                                        {{ $detail->fk_barang ? $detail->fk_barang->satuan : "PCS" }}
+                                        {{ $detail->fk_barang ? $detail->fk_barang->satuan : 'PCS' }}
                                         {{ $detail->kode_barang }}
                                         {{ $detail->fk_barang ? $detail->fk_barang->nama_barang : $detail->kode_barang }}
                                     </td>
                                     <td class="text-end border">Rp. {{ number_format($detail->harga_barang) }}</td>
-                                    <td class="text-end border">Rp. {{ number_format(($detail->jumlah_barang * $detail->harga_barang)) }}</td>
+                                    <td class="text-end border">Rp.
+                                        {{ number_format($detail->jumlah_barang * $detail->harga_barang) }}</td>
                                     @php
                                         $subTotal += $detail->jumlah_barang * $detail->harga_barang;
                                         $grandTotal += $detail->jumlah_barang * $detail->harga_barang;
@@ -34,11 +36,13 @@
                     @endforeach
                     <tr class="font-bold ">
                         <td class="border bg-green-300 border-green-400 py-3">Sub Total</td>
-                        <td class="text-end border bg-green-300 border-green-400" colspan="2">Rp. {{ number_format($subTotal) }}</td>
+                        <td class="text-end border bg-green-300 border-green-400" colspan="2">Rp.
+                            {{ number_format($subTotal) }}</td>
                     </tr>
                     <tr class="font-bold ">
                         <td class="border bg-green-300 border-green-400 py-3">Grand Sub Total</td>
-                        <td class="text-end border bg-green-300 border-green-400" colspan="2">Rp. {{ number_format($grandTotal) }}</td>
+                        <td class="text-end border bg-green-300 border-green-400" colspan="2">Rp.
+                            {{ number_format($grandTotal) }}</td>
                     </tr>
                 </table>
             </div>
